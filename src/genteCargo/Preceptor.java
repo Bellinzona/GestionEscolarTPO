b3145c69c3a3c291643f19dcf00b78ef13136b38
Tannacondia <95078436+Tannacondia@users.noreply.github.com>
2024-06-07 11:27:39 -0300

    Add files via upload

---
+++ src/genteCargo/Preceptor.java
@@ -0,0 +1,41 @@
+package genteCargo;
+
+public class Preceptor {
+    private String nombre;
+
+    private int legajo;
+    private String tarea;
+
+    public String getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(String tarea) {
+        this.tarea = tarea;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+    public String mostrarTreaAsignada(String tarea){
+        return tarea;
+    }
+    public Preceptor(String nombre, int legajo, String tarea){
+        this.nombre=nombre;
+        this.legajo=legajo;
+        this.tarea=tarea;
+
+    }
+}
